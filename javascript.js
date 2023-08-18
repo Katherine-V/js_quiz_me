2bb147babd13c6965fcb689e01582ee8d00aadfd
Katherine V <msvogler919@gmail.com>
2023-08-17 22:47:53 -0400

    add first javascript

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//array can include question and answer options
+//click start and timer begins
+//question appears
+//click answer && correct && new question || incorrect and lose 15 seconds time
+//no new questions || time runs out = game over
+//option to save initials and score
+
+
+document.getElementById("ques1").innerHTML="click!";
+//.addEventListener(click, function())
+
+var questions=[ title, choices, answer]
+var currentQuestion
+var quiz-questions=[//array of questions or at least numbers]
+
+for (var i=0; i<5; i++)
+    console.log(quiz-questions[i])
+
+
+var highscores= JSON.parse(window.localStorage).getitem("highscores")
