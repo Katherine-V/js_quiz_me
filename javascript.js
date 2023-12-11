52ef23cd6aaf37aaf222af09b486496eee47dce7
Katherine-V <msvogler919@gmail.com>
2023-12-11 11:00:28 -0500

    adding function
    putting notes into functional order

--- javascript.js
+++ javascript.js
@@ -18,3 +18,69 @@ for (var i=0; i<5; i++)
 
 
 var highscores= JSON.parse(window.localStorage).getitem("highscores")
+
+
+  <script>
+    const questions = [
+      {
+        question: "What is the capital of France?",
+        options: ["Paris", "London", "Berlin", "Madrid"],
+        correctAnswer: "Paris"
+      },
+      {
+        question: "What is the largest planet in our solar system?",
+        options: ["Earth", "Jupiter", "Mars", "Venus"],
+        correctAnswer: "Jupiter"
+      },
+      // Add more questions as needed
+    ];
+
+    let currentQuestionIndex = 0;
+    let timer = 60;
+
+    function startQuiz() {
+      displayQuestion();
+      setInterval(updateTimer, 1000);
+    }
+
+    function displayQuestion() {
+      const currentQuestion = questions[currentQuestionIndex];
+      document.getElementById('question-container').innerHTML = currentQuestion.question;
+      document.getElementById('options-container').innerHTML = currentQuestion.options.map(option =>
+        `<input type="radio" name="answer" value="${option}"> ${option}<br>`
+      ).join('');
+    }
+
+    function updateTimer() {
+      timer--;
+      document.getElementById('time').textContent = timer;
+      if (timer === 0) {
+        alert("Time's up! Quiz Over.");
+        // Add code for handling end of quiz
+      }
+    }
+
+    function nextQuestion() {
+      const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+
+      if (selectedAnswer) {
+        if (selectedAnswer.value === questions[currentQuestionIndex].correctAnswer) {
+          alert("Correct!");
+        } else {
+          alert("Incorrect!");
+        }
+
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length) {
+          displayQuestion();
+        } else {
+          alert("Quiz Over!");
+          // Add code for handling end of quiz
+        }
+      } else {
+        alert("Please select an answer!");
+      }
+    }
+
+    startQuiz();
+  </script>
